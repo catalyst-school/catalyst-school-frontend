fbd67a30667f8e5ee0796fec9a2ceda32420a08e
Tim Shenin <t.shenin@celonis.com>
2022-05-08 18:31:15 +0200

    Run yarn lint on everything

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
     root: true,
-    extends: ['plugin:vue/vue3-recommended', 'prettier'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-recommended',
+        '@vue/eslint-config-prettier',
+        '@vue/eslint-config-typescript',
+    ],
     env: {
         'vue/setup-compiler-macros': true,
     },
