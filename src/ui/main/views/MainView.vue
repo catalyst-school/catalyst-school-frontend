6d021be82889cf4af6b50e601bbfa508210dfa88
Tim Shenin <t.shenin@celonis.com>
2022-08-22 21:39:47 +0200

    Routing for home page

--- src/ui/main/views/MainView.vue
+++ src/ui/main/views/MainView.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import MainMenu from '../components/MainMenu.vue';
-import UserPath from '../components/UserPath.vue';
 </script>
 
 <template>
     <MainMenu />
     <main>
-        <UserPath />
+        <RouterView />
     </main>
 </template>
 
